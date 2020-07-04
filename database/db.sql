edfd2ce1b0a595b4a99a6bb7ed2b662aa8a5e879
RudyRosa0318 <51072737+RudyRosa0318@users.noreply.github.com>
2020-07-04 15:43:25 -0600

    Modficiacion de Tablas
    Se modifican y crean algunas tablas necesarias para la base de datos y la pagina web.

--- database/db.sql
+++ database/db.sql
@@ -1,34 +1,61 @@
-CREATE DATABASE links;
-USE links;
+CREATE DATABASE db_tech2u;
+USE db_tech2u;
 
 CREATE TABLE users (
-  id INT(11) NOT NULL,
-  username VARCHAR(16) NOT NULL,
+  idUser INT(11) NOT NULL,
+  userName VARCHAR(16) NOT NULL,
   password VARCHAR(60) NOT NULL,
-  fullname VARCHAR(100) NOT NULL
+  fullName VARCHAR(100) NOT NULL,
+  userType INT(2) NOT NULL
 );
 ALTER TABLE users
-  ADD PRIMARY KEY (id);
+  ADD PRIMARY KEY (idUser);
 
 ALTER TABLE users
-  MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+  MODIFY idUser INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 DESCRIBE users;
  
--- LINKS TABLE
-CREATE TABLE links (
+-- CATEGORIES TABLE
+CREATE TABLE categories (
   id INT(11) NOT NULL,
   title VARCHAR(150) NOT NULL,
   url VARCHAR(255) NOT NULL,
-  description TEXT,
-  user_id INT(11),
-  created_at timestamp NOT NULL DEFAULT current_timestamp,
-  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+  description TEXT
 );
-ALTER TABLE links
+ALTER TABLE categories
   ADD PRIMARY KEY (id);
 
-ALTER TABLE links
+ALTER TABLE categories
   MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
-DESCRIBE links;
+DESCRIBE categories;
+
+-- PRODUCTS TABLE
+CREATE TABLE products (
+  idProduct INT(11) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  description TEXT,
+  price DOUBLE NOT NULL,
+  productStatus VARCHAR(50) NOT NULL ,
+  quantity INT(5) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT current_timestamp,
+  PRIMARY KEY (idProduct)
+);
+
+ALTER TABLE products
+  MODIFY idProduct INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+---------------
+
+--PAGE USER TABLE
+CREATE TABLE pageUsers(
+  idUser INT(11) NOT NULL,
+  userName VARCHAR(16) NOT NULL,
+  password VARCHAR(60) NOT NULL,
+  name VARCHAR (30) NOT NULL,
+  lastName VARCHAR (30) NOT NULL,
+  PRIMARY KEY (idUser)
+);
+
+ALTER TABLE pageUsers
+  MODIFY idUser INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
