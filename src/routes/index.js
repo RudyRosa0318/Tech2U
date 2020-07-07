17008122a0df45cb71452942789cca912336e93c
oscartulio <51069335+oscartulio@users.noreply.github.com>
2020-07-07 00:24:28 -0600

    correcion de bugs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {rendex} = require('../controller/index.controller');
 
-router.get('/index', rendex);
-
+//router.get('/index', rendex);
+router.get('/',(req, res ) => {
+    res.render('/index');
+})
 module.exports = router;
