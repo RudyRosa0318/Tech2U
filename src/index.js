1660a712f8d908b15049a95dc3170f2cf53d58d3
RudyRosa0318 <51072737+RudyRosa0318@users.noreply.github.com>
2020-07-16 03:25:39 -0600

    Revert "Correcion Inde y Verificacion de Imagenes con extension"
    This reverts commit 36643610ab3f0c8856fec27c980c7f8d75e5df41.

--- src/index.js
+++ src/index.js
@@ -66,17 +66,7 @@ next();
  });
 //configurando parametros de almacenamiento multer, solo aceptara una imagen.
  app.use(multer({
-   storage,
-   //este es un filtro para decir que solo sube imagenes con las extensiones deseadas
-   fileFilter: (req,file,cb) =>{
-     const filetypes = /jpeg|gif|png|jpg/; 
-    const mimetype = filetypes.test(file.mimetype);
-    const extname = filetypes.test(path.extname(file.originalname));
-    if (mimetype && extname){
-      return cb(null,true);
-    }
-    cb("Esta no es una imagen Válida")
-   }
+   storage
  }).single('imagen'))
 
 
