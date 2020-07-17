7ed9cc2473a3e296fcb0a92931ac9668bc54a1b2
RudyRosa0318 <51072737+RudyRosa0318@users.noreply.github.com>
2020-07-17 00:51:44 -0600

    Correccion de Base de Datos y controlador

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,44 @@
 CREATE DATABASE db_tech2u;
 USE db_tech2u;
 
+CREATE TABLE category(
+  idCategory INT(11) NOT NULL,
+  name VARCHAR (50) NOT NULL,
+  PRIMARY KEY (idCategory)
+);
+
+-- PRODUCTS TABLE
+CREATE TABLE product (
+  idProduct INT(11) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  description TEXT,
+  price DECIMAL(10,2) NOT NULL,
+  idCategory INT(11) NOT NULL,
+  url_image VARCHAR(100) NULL,
+  idImage VARCHAR(30) NULL,
+  update_at datetime NULL,
+  created_at timestamp NOT NULL DEFAULT current_timestamp,
+  CONSTRAINT fk_category FOREIGN KEY(idCategory) REFERENCES category(idCategory),
+  PRIMARY KEY (idProduct)
+);
+
+ALTER TABLE product
+  MODIFY idProduct INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+
+--CART TABLE
+CREATE TABLE cart(
+  idCart INT(11) NOT NULL,
+  idProduct INT(11) NOT NULL,
+  qty INt(10),
+  total DECIMAL(10,2),  
+  CONSTRAINT fk_product FOREIGN KEY(idProduct) REFERENCES product(idProduct),
+  PRIMARY KEY (idCart)
+);
+
+ALTER TABLE cart
+  MODIFY idCart INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
 --Table Users
 CREATE TABLE users (
   idUser INT(11) NOT NULL,
@@ -19,38 +57,8 @@ MODIFY idUser INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 DESCRIBE users;
 
 
--- PRODUCTS TABLE
-CREATE TABLE product (
-  idProduct INT(11) NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  description TEXT,
-  price DECIMAL(10,2) NOT NULL,
-  categoryid INT(11) NOT NULL,
-  url_image VARCHAR(100) NULL,
-  update_at datetime NULL,
-  created_at timestamp NOT NULL DEFAULT current_timestamp,
-  PRIMARY KEY (idProduct)
-);
 
-ALTER TABLE product
-  MODIFY idProduct INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
-ALTER TABLE product
-ADD idImage VARCHAR(30),
-ADD ;
-
---CART TABLE
-CREATE TABLE cart(
-  idCart INT(11) NOT NULL,
-  idProduct INT(11) NOT NULL,
-  qty INt(10),
-  total DECIMAL(10,2),  
-  CONSTRAINT fk_product FOREIGN KEY(idProduct) REFERENCES product(idProduct),
-  PRIMARY KEY (idCart)
-);
-
-ALTER TABLE cart
-  MODIFY idCart INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 
 
