10f2116a4412f9c73e6fe3694a970f68c6e5ae70
MarioArgueta <51066024+MarioArgueta@users.noreply.github.com>
2020-07-21 16:00:45 -0600

    Formulario de pago con Stripe
    Aun falta solucionar errores.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const stripe = require("stripe")('sk_test_51H5lSsKfonblX5qIvwoTUBUJouItcvUbTLKlo2Ac7dlzybifJW1n7kj6XESVzhmSyS1p554Tf8SwAsLRnZvpIRAQ00T0PnH3hM');
 
 const {
   renderIndex,
@@ -12,5 +13,22 @@ router.get("/", renderIndex);
 router.get("/description", renderDescription);
 router.get("/description/:id", obtenerProductoPorId);
 //  router.get('/',indexc);
+
+router.post("/checkout", async (req, res)=>{
+  console.log(req.body);
+  const custumer = await stripe.custumers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  });
+  const charge = stripe.charges.create({
+    //amount: '',
+    //currency: 'usd',
+    custumer: custumer.id,
+    //description: '' 
+  })
+  console.log(custumer);
+  console.log(charge.id);
+  res.send('Recibido');
+})
 
 module.exports = router;
