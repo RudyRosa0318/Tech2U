d5ab75aca8caa38017a509baa5d492399a4954e8
RudyRosa0318 <51072737+RudyRosa0318@users.noreply.github.com>
2020-07-23 01:45:55 -0600

    Ahora los productos muestra su categoria

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -1,10 +1,11 @@
 const indexc = {};
 const pool = require("../model/database");
 
-indexc.renderIndex = async(req, res) => {
+indexc.renderIndex = async (req, res) => {
   const products = await pool.query("SELECT * FROM product");
-  const category = await pool.query("SELECT * FROM category")
-    res.render("index",{products,category});
+  const category = await pool.query("SELECT * FROM category");
+  
+  res.render("index", { products, category });
 };
 indexc.renderDescription = (req, res) => {
   res.render("products/description");
@@ -13,17 +14,16 @@ indexc.renderCart = (req, res) => {
   res.render("links/cart");
 };
 
-
-
 indexc.obtenerProductoPorId = async (req, res, next) => {
-  const  {id} = req.params;
+  const { id } = req.params;
   try {
-    const links = await pool.query("SELECT * FROM product WHERE idProduct = ?", [
-      id,
-    ]);
+    const links = await pool.query(
+      "SELECT C.name AS category, P.idProduct, P.name, P.description, P.price, P.idCategory, P.url_image,P.idImage,P.created_at,P.update_at FROM product AS P INNER JOIN category AS C ON p.idCategory = C.idCategory WHERE idProduct = ?",
+      [id]
+    );
     console.log(links[0]);
     res.render("products/description", {
-      links:links[0]
+      links: links[0],
     });
   } catch (error) {
     // res.redirect("/");
--- src/controller/links.controller.js
+++ src/controller/links.controller.js
@@ -33,7 +33,7 @@ res.addtheLink = async (req, res) => {
 };
 
 res.renderLinks = async (req, res) => {
-  const links = await pool.query("SELECT * FROM product");
+  const links = await pool.query("SELECT C.name AS category, P.idProduct, P.name, P.description, P.price, P.idCategory, P.url_image,P.idImage,P.created_at,P.update_at FROM product AS P INNER JOIN category AS C ON p.idCategory = C.idCategory");
 
   res.render("links/list", { links });
 };
