3263dd8e170994b736460e134cb0fb74ec88b046
RudyRosa0318 <51072737+RudyRosa0318@users.noreply.github.com>
2020-07-29 21:32:18 -0600

    Correccion de Editar y de Index

--- src/controller/links.controller.js
+++ src/controller/links.controller.js
@@ -1,5 +1,7 @@
 const pool = require("../model/database");
-
+const { unlink } = require("fs-extra");
+const paths = require("path");
+const { ifError } = require("assert");
 const res = {};
 
 res.AddLink = async (req, res) => {
@@ -27,6 +29,8 @@ res.addtheLink = async (req, res) => {
       idImage: filename,
       url_image: "/img/upload/" + filename,
     };
+    await pool.query("INSERT INTO product set ?", [newLink]);
+    await pool.query("INSERT INTO image set ?", [newImage]);
   } else {
     const newLink = {
       name,
@@ -35,9 +39,9 @@ res.addtheLink = async (req, res) => {
       idCategory,
       url_image,
     };
+    await pool.query("INSERT INTO product set ?", [newLink]);
   }
-  await pool.query("INSERT INTO product set ?", [newLink]);
-  await pool.query("INSERT INTO image set ?", [newImage]);
+
   req.flash("success", "Guardado correctamente!");
   res.redirect("/links");
 };
@@ -70,21 +74,117 @@ res.renderEditLink = async (req, res) => {
 
 res.editLink = async (req, res) => {
   const { idProduct } = req.params;
-  const { filename } = req.file;
-  const { name, price, description, idCategory, url_image } = req.body;
-  const newLink = {
-    name,
-    price,
-    description,
-    idCategory,
-    url_image: "/img/upload/" + filename,
-  };
-  await pool.query("UPDATE product set ? WHERE idProduct = ?", [
-    newLink,
-    idProduct,
-  ]);
-  req.flash("success", "Editado correctamente!");
-  res.redirect("/links");
-};
+  const { name, description, price, idCategory } = req.body;
 
+  if (req.file) {
+    const { filename, originalname, mimetype, size, path } = req.file;
+    const product = await pool.query(
+      "SELECT * from product WHERE idProduct = ?",
+      [idProduct]
+    );
+    const { idImage, url_image } = product[0];
+    if (idImage) {
+      await unlink(paths.resolve("./src/public" + url_image));
+      const newImage = {
+        filename,
+        originalname,
+        mimetype,
+        size,
+        path: "/img/upload/" + filename,
+      };
+      await pool.query("UPDATE image set ? WHERE idImage = ?", [
+        newImage,
+        idImage,
+      ]);
+    } else {
+      const newImage = {
+        idImage: filename,
+        filename,
+        originalname,
+        mimetype,
+        size,
+        path: "/img/upload/" + filename,
+      };
+      await pool.query("INSERT INTO image set ?", [newImage]);
+    }
+
+    const newLink = {
+      name,
+      price,
+      description,
+      idCategory,
+      url_image: "/img/upload/" + filename,
+    };
+
+    await pool.query("UPDATE product set ? WHERE idProduct = ?", [
+      newLink,
+      idProduct,
+    ]);
+
+    req.flash("success", "Editado correctamente!");
+    res.redirect("/links");
+  } else {
+    const url = req.body.url_image;
+    if (url) {
+      const product = await pool.query(
+        "SELECT * from product WHERE idProduct = ?",
+        [idProduct]
+      );
+      const { idImage, url_image } = product[0];
+
+      if (existeImagen(idImage)) {
+        const { idProduct } = req.params;
+        await unlink(paths.resolve("./src/public" + url_image));
+
+        // Borrar la imagen de la base
+        await pool.query("DELETE from image WHERE idImage = ?", [idImage]);
+        // Actualizar product
+        const newLink = {
+          name,
+          description,
+          price,
+          idCategory,
+          url_image: url,
+        };
+        await pool.query("UPDATE product set ? WHERE idProduct = ?", [newLink,idProduct]);
+        req.flash("success", "Editado correctamente!");
+        res.redirect("/links");
+      } else {
+        const { idProduct } = req.params;
+        const newLink = {
+          name,
+          description,
+          price,
+          idCategory,
+          url_image: url,
+        };
+        await pool.query("UPDATE product set ? WHERE idProduct = ?", [newLink,idProduct]);
+        req.flash("success", "Editado correctamente!");
+        res.redirect("/links");
+      }
+    } else {
+      const { idProduct } = req.params;
+      const newLink = {
+        name,
+        price,
+        description,
+        idCategory,
+      };
+      await pool.query("UPDATE product set ? WHERE idProduct = ?", [
+        newLink,
+        idProduct,
+      ]);
+      req.flash("success", "Editado correctamente!");
+      res.redirect("/links");
+    }
+  }
+};
+async function existeImagen(id) {
+  const image = await pool.query("SELECT * from Image WHERE idImage = ?", [id]);
+  if (image) {
+    return true;
+  } else {
+    return false;
+  }
+}
 module.exports = res;
--- src/public/js/modules/producto.js
+++ src/public/js/modules/producto.js
@@ -2,22 +2,23 @@ const botones = document.querySelector('input[name="toggle"]');
 const url = document.getElementById("url");
 const archivo = document.getElementById("customFileLangHTML");
 const l_archivo = document.getElementById("l_customFileLangHTML");
-archivo.style.display = "none";
-l_archivo.style.display = "none";
-botones.addEventListener("click", (e) => {
-  const cbx = document.getElementById(`customSwitch_t`);
-  const label_t = document.getElementById(`label_t`);
+if (botones) {
+  archivo.style.display = "none";
+  l_archivo.style.display = "none";
+  botones.addEventListener("click", (e) => {
+    const cbx = document.getElementById(`customSwitch_t`);
+    const label_t = document.getElementById(`label_t`);
 
-  console.log(archivo);
-  if (cbx.checked) {
-    label_t.innerHTML = "ARCHIVO";
-    url.style.display = "none";
-    archivo.style.display = "block";
-    l_archivo.style.display = "block";
-  } else {
-    label_t.innerHTML = "URL";
-    url.style.display = "block";
-    archivo.style.display = "none";
-    l_archivo.style.display = "none";
-  }
-});
+    if (cbx.checked) {
+      label_t.innerHTML = "ARCHIVO";
+      url.style.display = "none";
+      archivo.style.display = "block";
+      l_archivo.style.display = "block";
+    } else {
+      label_t.innerHTML = "URL";
+      url.style.display = "block";
+      archivo.style.display = "none";
+      l_archivo.style.display = "none";
+    }
+  });
+}
