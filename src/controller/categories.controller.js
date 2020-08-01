c20630c64d77abb6f543ecff5f8e03c4d638fb34
Danny Joaquin <50885486+JoaquinHN@users.noreply.github.com>
2020-08-01 09:44:52 -0600

    CRUD completo de tabla category

--- src/controller/categories.controller.js
+++ src/controller/categories.controller.js
@@ -4,11 +4,11 @@ const hud = {};
 
 hud.AddCat = (req, res) => {
     const carrito = req.session.cart;
-    res.render("categories/add",{carrito});
+    res.render("categories/add", { carrito });
 };
 
 hud.AddTheCat = async (req, res) => {
-    const {name} = req.body;
+    const { name } = req.body;
     const newCat = {
         name,
     }
@@ -21,6 +21,27 @@ hud.renderCat = async (req, res) => {
     const cate = await pool.query("SELECT * FROM category");
     const carrito = req.session.cart;
     res.render("categories/list", { cate, carrito });
+};
+hud.renderEditCat = async (req, res) => {
+    const {idCategory } = req.params;
+    const category = await pool.query("SELECT * FROM category WHERE idCategory = ?", [idCategory]);
+    res.render("categories/edit", { category: category[0]});
+};
+hud.editCat = async (req, res) => {
+    const { idCategory } = req.params;
+    const {name} = req.body;
+    const nameUP = {
+        name
+    };
+    await pool.query("UPDATE category set ? WHERE idCategory = ?", [nameUP, idCategory]);
+    res.redirect("/categories");
+};
+
+hud.deleteCat = async (req, res) => {
+    const { idCategory } = req.params;
+    await pool.query("DELETE FROM category WHERE idCategory = ?", [idCategory]);
+    req.flash("success", "Eliminado correctamente!");
+    res.redirect("/categories");
   };
 
 module.exports = hud;
