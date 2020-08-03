90c43faa5fcc824d63e4840120c310ee5a4164e2
RudyRosa0318 <51072737+RudyRosa0318@users.noreply.github.com>
2020-08-03 01:41:15 -0600

    Probando problemas con conexion commit

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 module.exports = {
   database: {
-    connectionLimit: 100,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
