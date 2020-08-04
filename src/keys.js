1d840277e752e0914e019530baa4ac7dc31cd253
oscartulio <51069335+oscartulio@users.noreply.github.com>
2020-08-03 21:10:57 -0600

    Correción de texto email

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 module.exports = {
   database: {
+    connectionLimit: 100,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
